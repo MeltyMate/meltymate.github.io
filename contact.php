98e72fa156e8785daa4fef8f6f15d0594b6389b7
MeltyMate <mate.mirkovszki@myy.haaga-helia.fi>
2023-12-04 16:22:59 +0200

    i made will

--- contact.php
+++ contact.php
@@ -3,10 +3,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    $to = "mate.mirkovszkil@gmail.";
+    $to = "mate.mirkovszkil@gmail.com"; // Replace with your actual email address
     $subject = "New Contact Form Submission";
     $headers = "From: $email";
 
-    mail($to, $subject, $message, $headers);
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Failed to send email. Please try again.";
+    }
 }
->
+?>
