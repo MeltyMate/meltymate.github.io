27f7faa1a3d724fac697da58098faae77af18975
MeltyMate <144317662+MeltyMate@users.noreply.github.com>
2023-12-09 14:10:06 +0200

    Update website.js

--- website.js
+++ website.js
@@ -1,39 +1,54 @@
-</html>   document.addEventListener("DOMContentLoaded", function () {
-  highlightActivePage();
+document.addEventListener("DOMContentLoaded", function () {
+  try {
+    highlightActivePage();
+  } catch (error) {
+    console.error("Error in highlightActivePage:", error);
+  }
 });
 
 function highlightActivePage() {
-  var currentPage = document.body.id;
-  var currentLink = document.querySelector(`.flex-container a[href='${currentPage}.html']`);
-  if (currentLink) {
-    currentLink.classList.add("active");
+  try {
+    var currentPage = document.body.id;
+    var currentLink = document.querySelector(`.flex-container a[href='${currentPage}.html']`);
+    if (currentLink) {
+      currentLink.classList.add("active");
+    }
+  } catch (error) {
+    console.error("Error in highlightActivePage:", error);
   }
 }
 
 function openGallery(category) {
-  let galleryPageURL;
+  try {
+    let galleryPageURL;
 
-  switch (category) {
-    case 'goKart':
-      galleryPageURL = 'https://meltymate.github.io/GallerygoKart.html';
-      break;
-    case 'Sim':
-      galleryPageURL = 'https://meltymate.github.io/GallerySim.html';
-      break;
-    case 'Motorbike':
-      galleryPageURL = 'https://meltymate.github.io/GalleryMotorbike.html';
-      break;
-    
-    case 'rc':
-      galleryPageURL = "https://meltymate.github.io/GalleryRC.html";
-      break;
-  }
+    switch (category) {
+      case 'goKart':
+        galleryPageURL = 'https://meltymate.github.io/GallerygoKart.html';
+        break;
+      case 'Sim':
+        galleryPageURL = 'https://meltymate.github.io/GallerySim.html';
+        break;
+      case 'Motorbike':
+        galleryPageURL = 'https://meltymate.github.io/GalleryMotorbike.html';
+        break;
+      case 'rc':
+        galleryPageURL = "https://meltymate.github.io/GalleryRC.html";
+        break;
+    }
 
-  if (galleryPageURL) {
-    window.location.href = galleryPageURL;
+    if (galleryPageURL) {
+      window.location.href = galleryPageURL;
+    }
+  } catch (error) {
+    console.error("Error in openGallery:", error);
   }
 }
 
 function toggleImageSize(img) {
-  img.classList.toggle('enlarged');
-} doesnt work with that page
+  try {
+    img.classList.toggle('enlarged');
+  } catch (error) {
+    console.error("Error in toggleImageSize:", error);
+  }
+}
