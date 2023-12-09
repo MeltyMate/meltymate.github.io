e0cccaecad673201aa439cc89ad38b88c09daefd
MeltyMate <144317662+MeltyMate@users.noreply.github.com>
2023-12-09 12:06:18 +0200

    Update website.js

--- website.js
+++ website.js
@@ -1,8 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var currentPage = document.body.id;
-    var currentLink = document.querySelector(`.flex-container a[href='${currentPage}.html']`);
-    if (currentLink) {
-      currentLink.classList.add("active");
-    }
-  });
-  
+  var currentPage = document.body.id;
+  var currentLink = document.querySelector(`.flex-container a[href='${currentPage}.html']`);
+  if (currentLink) {
+    currentLink.classList.add("active");
+  }
+});
+
+function openGallery(category) {
+  // Construct the gallery page URL based on the selected category
+  const galleryPageURL = `gallery${category}.html`;
+
+  // Navigate to the constructed gallery page
+  window.location.href = galleryPageURL;
+}
+
+function toggleImageSize(img) {
+  img.classList.toggle('enlarged');
+}
